1f07ee71d56923cbde4c16be3b475fd3c1c2d49d
Gino Ochoa <gochoa1088@gmail.com>
2022-10-26 18:10:00 -0700

    accepting body data

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,30 +1,15 @@
-// @desc     Get Goals
-// @route    GET /api/goals
-// @access   Private
-
 const getGoals = (req, res) => {
   res.status(200).json({ message: "Get goals!" });
 };
 
-// @desc     Set Goal
-// @route    POST /api/goals
-// @access   Private
-
 const setGoal = (req, res) => {
+  console.log(req.body);
   res.status(200).json({ message: "Set goal!" });
 };
-
-// @desc     Update Goal
-// @route    PUT /api/goals/:id
-// @access   Private
 
 const updateGoal = (req, res) => {
   res.status(200).json({ message: `Update goal ${req.params.id}!` });
 };
-
-// @desc     Delete Goal
-// @route    DELETE /api/goals/:id
-// @access   Private
 
 const deleteGoal = (req, res) => {
   res.status(200).json({ message: `Deleted goal ${req.params.id}!` });
