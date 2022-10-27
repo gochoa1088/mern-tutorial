db362a9f9cec37eff050a5b56d43c2d019dcfebd
Gino Ochoa <gochoa1088@gmail.com>
2022-10-26 18:00:05 -0700

    creating controller functions

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,5 +1,38 @@
+// @desc     Get Goals
+// @route    GET /api/goals
+// @access   Private
+
 const getGoals = (req, res) => {
   res.status(200).json({ message: "Get goals!" });
 };
 
-module.exports = {};
+// @desc     Set Goal
+// @route    POST /api/goals
+// @access   Private
+
+const setGoal = (req, res) => {
+  res.status(200).json({ message: "Set goal!" });
+};
+
+// @desc     Update Goal
+// @route    PUT /api/goals/:id
+// @access   Private
+
+const updateGoal = (req, res) => {
+  res.status(200).json({ message: `Update goal ${req.params.id}!` });
+};
+
+// @desc     Delete Goal
+// @route    DELETE /api/goals/:id
+// @access   Private
+
+const deleteGoal = (req, res) => {
+  res.status(200).json({ message: `Deleted goal ${req.params.id}!` });
+};
+
+module.exports = {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+};
