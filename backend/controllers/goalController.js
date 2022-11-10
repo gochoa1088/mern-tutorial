2cc89c1f1faa68c1485700452b0813d7ccb52627
Gino Ochoa <gochoa1088@gmail.com>
2022-11-09 16:36:10 -0800

    update goal

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,6 +1,6 @@
 const asyncHandler = require("express-async-handler");
 
-const Goal = require("../models/goalModel")
+const Goal = require("../models/goalModel");
 
 const getGoals = asyncHandler(async (req, res) => {
   const goals = await Goal.find();
@@ -15,14 +15,25 @@ const setGoal = asyncHandler(async (req, res) => {
   }
 
   const goal = await Goal.create({
-    text: req.body.text
-  })
+    text: req.body.text,
+  });
 
   res.status(200).json(goal);
 });
 
 const updateGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update goal ${req.params.id}!` });
+  const goal = await Goal.findById(req.params.id);
+
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not found.");
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  res.status(200).json(updatedGoal);
 });
 
 const deleteGoal = asyncHandler(async (req, res) => {
--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -1,12 +1,15 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const goalSchema = mongoose.Schema({
+const goalSchema = mongoose.Schema(
+  {
     text: {
-        type: String,
-        required: [true, "Please add a text value."],
-    }
-}, {
-    timestamps: true
-})
+      type: String,
+      required: [true, "Please add a text value."],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model("Goal", goalSchema)
+module.exports = mongoose.model("Goal", goalSchema);
