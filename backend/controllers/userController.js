a72ef4e609a1a4f26ceade4dc8cc8f907d1b8a13
Gino Ochoa <gochoa1088@gmail.com>
2022-11-10 13:21:14 -0800

    hash password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -18,7 +18,25 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error("User already exists.");
   }
 
-  res.json({ message: "Register user." });
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data.");
+  }
 });
 
 const loginUser = asyncHandler(async (req, res) => {
