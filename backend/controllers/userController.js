8f2fbfcc3d0898a9c9e70dabbbeed5b9aaffccb2
Gino Ochoa <gochoa1088@gmail.com>
2022-11-10 13:12:10 -0800

    user routes and controller

---
+++ backend/controllers/userController.js
@@ -0,0 +1,17 @@
+const registerUser = (req, res) => {
+  res.json({ message: "Register user." });
+};
+
+const loginUser = (req, res) => {
+  res.json({ message: "Login user." });
+};
+
+const getMe = (req, res) => {
+  res.json({ message: "User data." });
+};
+
+module.exports = {
+  registerUser,
+  loginUser,
+  getMe,
+};
