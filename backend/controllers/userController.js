ddded97232176b625c9099c8750a24dd4e8128b7
Gino Ochoa <gochoa1088@gmail.com>
2022-11-10 13:16:54 -0800

    register user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,14 +1,33 @@
-const registerUser = (req, res) => {
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please add all fields.");
+  }
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists.");
+  }
+
   res.json({ message: "Register user." });
-};
+});
 
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
   res.json({ message: "Login user." });
-};
+});
 
-const getMe = (req, res) => {
+const getMe = asyncHandler(async (req, res) => {
   res.json({ message: "User data." });
-};
+});
 
 module.exports = {
   registerUser,
