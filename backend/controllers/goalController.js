c1429a1e935c63396ef8df472582ff3bc483e14f
Gino Ochoa <gochoa1088@gmail.com>
2022-11-09 16:27:56 -0800

    getting and setting goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,7 +1,11 @@
 const asyncHandler = require("express-async-handler");
 
+const Goal = require("../models/goalModel")
+
 const getGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Get goals!" });
+  const goals = await Goal.find();
+
+  res.status(200).json(goals);
 });
 
 const setGoal = asyncHandler(async (req, res) => {
@@ -10,7 +14,11 @@ const setGoal = asyncHandler(async (req, res) => {
     throw new Error("Please add a text field!");
   }
 
-  res.status(200).json({ message: "Set goal!" });
+  const goal = await Goal.create({
+    text: req.body.text
+  })
+
+  res.status(200).json(goal);
 });
 
 const updateGoal = asyncHandler(async (req, res) => {
